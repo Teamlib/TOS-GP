60cb43399e5652edc08a67b973a3b0a1a348112b
Resked <taratynov.99@mail.ru>
2018-12-18 15:59:16 +0700

    localDB

--- TOS/TOS/MainWindow.xaml.cs
+++ TOS/TOS/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLitePCL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,42 @@ namespace TOS
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StructureButton_Click(object sender, RoutedEventArgs e)
+        {
+            //using (var conn = new SQLiteConnection("Storage.db"))
+            //{
+            //    string sql = @"CREATE TABLE IF NOT EXISTS People (
+            //                                    ID INTEGER NOT NULL PRIMARY KEY,
+            //                                    FirstName NVARCHAR(50),
+            //                                    LastName NVARCHUAR(50));";
+            //    using (var statement = conn.Prepare(sql))
+            //    {
+            //        statement.Step();
+            //    }
+            //}
+
+            ff.Text = null;
+
+            using (var conn = new SQLiteConnection("Storage.db"))
+            {
+                using (var statement = conn.Prepare("INSERT INTO People (FirstName, LastName) VALUES ('Конек', 'Горбунок')"))
+                {
+                    statement.Step();
+                }
+            }
+
+            using (var conn = new SQLiteConnection("Storage.db"))
+            {
+                using (var statement = conn.Prepare("SELECT LastName, FirstName FROM People"))
+                {
+                    while (statement.Step() == SQLiteResult.ROW)
+                    {
+                        ff.Text = ff.Text + (string)statement[0] + Environment.NewLine;
+                    }
+                }
+            }
         }
     }
 }
